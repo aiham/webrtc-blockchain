6ae0690f476319a669a15f531c8c746cb5a244a8
Aiham Hammami <aiham@aiham.net>
2017-06-24 21:49:58 +1000

    Add server to simply broadcast any signal received to all other connections

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const https = require('https');
+const helmet = require('helmet');
+const cors = require('cors');
+const socketIO = require('socket.io');
+
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || undefined;
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+const server = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key'), 'utf8'),
+  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.crt'), 'utf8'),
+}, app);
+
+const io = socketIO(server, { serveClient: false });
+io.on('connection', socket => {
+  socket.on('signal', data => {
+    socket.broadcast.emit('signal', data);
+  });
+});
+
+server.listen(port, host, () => {
+  const port = server.address().port;
+  const host = server.address().address;
+  console.log(`App listening at https://${host}:${port}`);
+});
